8e2cd8493f7b0e0c8e58786b1385bad47c265756
Javier de Mena Asenjo <javier.demena@iesdoctorbalmis.com>
2022-10-27 18:48:55 +0200

    Ejercicio acabado

---
+++ MemeMaker/BooleanToInt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    public class BooleanToInt: IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                if ((bool) value) return "2";
+                else return "0";
+            }
+            else return "0";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
