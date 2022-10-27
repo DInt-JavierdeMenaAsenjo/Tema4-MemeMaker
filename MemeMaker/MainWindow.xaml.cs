cd4eaa40da936c925d07ad0d9366223db1ed87b9
Javier de Mena Asenjo <javier.demena@iesdoctorbalmis.com>
2022-10-27 21:54:25 +0200

    Añadido boton "Guardar como..."

--- MemeMaker/MainWindow.xaml.cs
+++ MemeMaker/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,32 @@ namespace MemeMaker
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DescargarClick(object sender, RoutedEventArgs e)
+        {
+            RenderTargetBitmap bitmap = new RenderTargetBitmap((int)this.meme.ActualWidth, (int)this.meme.ActualHeight, 96, 96, PixelFormats.Pbgra32);
+            bitmap.Render(this.meme);
+
+            var dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.Title = "Guardar como..."; // instead of default "Save As"
+            dialog.FileName = "Meme"; // Filename will then be "select.this.directory"
+            dialog.Filter = "Imagenes | *.jpeg";
+            dialog.DefaultExt = "jpeg";
+            if (dialog.ShowDialog() == true)
+            {
+                string path = dialog.FileName;
+                // If user has changed the filename, create the new directory
+                using (FileStream stream = File.Create($"{path}"))
+                {
+                    JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+                    encoder.QualityLevel = 90;
+                    encoder.Frames.Add(BitmapFrame.Create(bitmap));
+                    encoder.Save(stream);
+                }
+            }
+
+            
         }
     }
 }
